0eba88f2c2e3bbb3b9931627532610d3fe7a13c2
Wlad loban <qwegadqwe@gmail.com>
2023-11-08 23:48:48 +0300

    test2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -30,6 +30,7 @@ export const App = observer(() => {
   return (
     <div className="App">
       <Routes>
+        <h2>main new commit</h2>
         <Route path="/" element={<TodolistList/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="*" element={<h1>404: PAGE NOT FOUND</h1>}/>
